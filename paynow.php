018959fd1c47c73da7549974eb61457a82dab7d4
Jurgens Banninga <jurgens@interexcel.co.za>
2015-08-19 13:28:43 +0200

    Add checks for EFT notification and allow for redirect

--- paynow.php
+++ paynow.php
@@ -6,35 +6,63 @@ require('./wp-load.php');
 $home_url = get_option('siteurl');
 $success = false;
 
-if( isset($_POST) && $_POST['TransactionAccepted'] == true ) {
-	// Success
-	$success = pn_sagepaynow_ipn();
-}
+function pn_do_transaction(){
 
-if( $success ) {
-	do_action('wpsc_payment_successful');
-	//die('SUCCESS!');
+    $accepted = isset($_POST['TransactionAccepted']) && $_POST['TransactionAccepted'] == true;
+    if( $accepted && pn_sagepaynow_ipn() ) {
 
-    echo "<p>Payment was successful.</p>";
-    echo "<p>You will be redirected to the site homepage in 5 seconds. <a href='$home_url'>Click here</a> to return immediately.</p>";
+        // Transaction successfull
+        do_action('wpsc_payment_successful');
+        //die('SUCCESS!');
+
+        echo "<p>Payment was successful.</p>";
+        echo "<p>You will be redirected to the site homepage in 5 seconds. <a href='$home_url'>Click here</a> to return immediately.</p>";
+        ?>
+
+        <script type="text/javascript">
+            setTimeout(function () {
+               window.location.href = '<?php echo $home_url; ?>';
+            }, 5000);
+        </script>
+        <?php
+        pnLog( 'Returning to clientarea.' );
     ?>
 
-    <script type="text/javascript">
-        setTimeout(function () {
-           window.location.href = '<?php echo $home_url; ?>';
-        }, 5000);
-    </script>
     <?php
-    pnLog( 'Returning to clientarea.' );
-?>
+    } else {
+        // Fail
+        do_action('wpsc_payment_failed');
+        // die('FAILED');
+        pnLog( 'Transaction Unsuccessful' );
 
-<?php
-} else {
-	// Fail
-	do_action('wpsc_payment_failed');
-	// die('FAILED');
-    pnLog( 'Transaction Unsuccessful' );
-
-    echo "<p>Payment was declined. Reason: " . $_POST['Reason'] . "</p>";
-    echo "<p><a href='$home_url'>Click here</a> to return to the store.</p>";
+        echo "<p>Payment was declined. Reason: " . $_POST['Reason'] . "</p>";
+        echo "<p><a href='$home_url'>Click here</a> to return to the store.</p>";
+    }
 }
+
+// $url_for_redirect = $home_url . '/your-account/';
+$url_for_redirect = ( get_option( 'user_account_url' ) );
+
+if( isset($_POST) && !empty($_POST) ) {
+
+    // This is the notification coming in OR the CC callback!
+    // Act as an IPN request and forward request to Credit Card method.
+    // Logic is exactly the same
+
+    pn_do_transaction();
+
+    die();
+
+} else {
+    // Probably calling the "redirect" URL
+
+    pnlog(__FILE__ . ' Probably calling the "redirect" URL');
+
+    if( $url_for_redirect ) {
+        header ( "Location: {$url_for_redirect}" );
+    } else {
+        die( "No 'redirect' URL set." );
+    }
+}
+
+die( PN_ERR_BAD_ACCESS );
